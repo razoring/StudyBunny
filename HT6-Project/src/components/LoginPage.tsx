0df7842cfb09192cd41412d82fac98343bffa6c2
aletha <aletha.moira@gmail.com>
2026-07-19 02:14:30 -0400

    UI fixes

--- HT6-Project/src/components/LoginPage.tsx
+++ HT6-Project/src/components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { AuthPage } from './AuthPage';
+import questStudyLogo from '../assets/quest_study_logo.png';
 
 export function LoginPage() {
   const { loginWithRedirect } = useAuth0();
@@ -7,6 +8,7 @@ export function LoginPage() {
   return (
     <AuthPage
       title="Quest Study"
+      logoSrc={questStudyLogo}
       primaryActionLabel="Log In"
       primaryAction={() => loginWithRedirect()}
       alternateTo="/signup"
